bc726eeee6b6dd78dcf1a273510baf7eed515333
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-09-24 18:38:01 -0300

    some new ideas

---
+++ new.js
@@ -0,0 +1,27 @@
+export default ({init, builder, view}) => {
+  var state
+  var isRunning = false
+
+  const dispatch = (event, ...args) => {
+    if (isRunning && typeof events[event] == 'function') {
+      events[event](...args)
+    }
+  }
+
+  const update = setter => {
+    if (isRunning) {
+      state = setter(state)
+      view(state, dispatch)
+    }
+  }
+
+  const events = builder(update, dispatch)
+  isRunning = true
+  update(() => init)
+  dispatch('init')
+
+  return () => {
+    dispatch('done')
+    isRunning = false
+  }
+}
